7d623a19bc39a85108d765b29a58a923b500bf33
dummy-andra <cursurifree@gmail.com>
2021-04-01 09:34:55 +0300

    add aro added

--- Azure/ARO/CreateCluster/deploy-private.sh
+++ Azure/ARO/CreateCluster/deploy-private.sh
@@ -11,47 +11,52 @@ az group create \
 # Create a virtual network
 az network vnet create \
    --resource-group $RESOURCEGROUP \
-   --name aro-vnet \
+   --name private-aro-vnet \
    --address-prefixes 10.0.0.0/22
 
 # Add an empty subnet for the master nodes
 az network vnet subnet create \
   --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
-  --name master-subnet \
+  --vnet-name private-aro-vnet \
+  --name private-master-subnet \
   --address-prefixes 10.0.0.0/23 \
   --service-endpoints Microsoft.ContainerRegistry
 
 # Add an empty subnet for the worker nodes
 az network vnet subnet create \
   --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
-  --name worker-subnet \
+  --vnet-name private-aro-vnet \
+  --name private-worker-subnet \
   --address-prefixes 10.0.2.0/23 \
   --service-endpoints Microsoft.ContainerRegistry
 
 # Disable subnet private endpoint policies on the master subnet. This is required for the service to be able to connect to and manage the cluster.
 az network vnet subnet update \
-  --name master-subnet \
+  --name private-master-subnet \
   --resource-group $RESOURCEGROUP \
-  --vnet-name aro-vnet \
+  --vnet-name private-aro-vnet \
   --disable-private-link-service-network-policies true
 
 
 # Create the cluster
-az aro create \
-  --resource-group $RESOURCEGROUP \
-  --name $CLUSTER \
-  --vnet aro-vnet \
-  --master-subnet master-subnet \
-  --worker-subnet worker-subnet \
-  --pull-secret @pull-secret.txt
-  #--domain foo.example.com
+
 
 # Connect to the cluster
 # az aro list-credentials \
 #   --name $CLUSTER \
 #   --resource-group $RESOURCEGROUP
+
+
+az aro create \
+  --resource-group $RESOURCEGROUP \
+  --name $CLUSTERprv \
+  --vnet private-aro-vnet \
+  --master-subnet private-master-subnet \
+  --worker-subnet private-worker-subnet \
+  --apiserver-visibility Private \
+  --ingress-visibility Private
+  # --domain foo.example.com # [OPTIONAL] custom domain
+  # --pull-secret @pull-secret.txt # [OPTIONAL]
 
 
 $ARO_Credentials=$(az aro list-credentials --name $CLUSTER --resource-group $RESOURCEGROUP )
--- Azure/ARO/CreateCluster/params.sh
+++ Azure/ARO/CreateCluster/params.sh
@@ -1,3 +1,4 @@
 LOCATION=eastus                 # the location of your cluster
-RESOURCEGROUP=aro-rg            # the name of the resource group where you want to create your cluster
-CLUSTER=cluster                 # the name of your cluster
+RESOURCEGROUP=v4-eastus           # the name of the resource group where you want to create your cluster
+CLUSTER=aro-cluster                 # the name of your cluster
+             
